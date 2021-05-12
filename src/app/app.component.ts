1b31b3632804922920f261633ba6c906d8f88851
Vigneshwaran16 <iamvignesh16@gmail.com>
2021-05-12 23:11:38 +0530

    GrapsJS Blocks

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import 'grapesjs-preset-webpage'
+import grapesjs from 'grapesjs'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+
+
+
+
+export class AppComponent implements OnInit{
   title = 'grapesjs-angular';
+  editor: any
+  ngOnInit(){
+
+    this.editor = grapesjs.init({
+      container: '#gjs',
+      fromElement: true,
+
+      height: '600px',
+      width:'auto',
+
+      storageManager: false,
+
+      panels: {
+        defaults: []
+      },
+
+      blockmanager : {
+        appendTo : '#blocks',
+        blocks: [
+          {
+          id: 'section',
+          label: 'Section',
+          attributes:{ class : 'gjs-block-section' },
+          content: `<section>
+                    <h3> Section Heading Test/h3>
+                    <p>Sample Line</p>
+                    </section>`,
+          },
+        ]
+      },
+    })
+
+    this.editor.getConfig().showDevices = false
+
+  }
 }
