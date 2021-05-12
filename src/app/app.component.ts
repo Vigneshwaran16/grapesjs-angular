f9b6f5e8aba1c9267a62d199367881ded1ab7675
Vigneshwaran16 <iamvignesh16@gmail.com>
2021-05-12 23:37:18 +0530

    GrapesJs Blocks

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -29,23 +29,54 @@ export class AppComponent implements OnInit{
         defaults: []
       },
 
-      blockmanager : {
-        appendTo : '#blocks',
+      blockManager: {
+        appendTo: '#blocks',
         blocks: [
           {
-          id: 'section',
-          label: 'Section',
-          attributes:{ class : 'gjs-block-section' },
-          content: `<section>
-                    <h3> Section Heading Test/h3>
-                    <p>Sample Line</p>
-                    </section>`,
-          },
+            id: 'section', // id is mandatory
+            label: '<b>Section</b>', // You can use HTML/SVG inside labels
+            attributes: { class:'gjs-block-section' },
+            content: `<section>
+              <h1>This is a simple title</h1>
+              <div>This is just a Lorem text: Lorem ipsum dolor sit amet</div>
+            </section>`,
+          }, {
+            id: 'text',
+            label: 'Text',
+            content: '<div data-gjs-type="text">Insert your text here</div>',
+          }, {
+            id: 'image',
+            label: 'Image',
+            // Select the component once it's dropped
+            select: true,
+            // You can pass components as a JSON instead of a simple HTML string,
+            // in this case we also use a defined component type `image`
+            content: { type: 'image' },
+            // This triggers `active` event on dropped components and the `image`
+            // reacts by opening the AssetManager
+            activate: true,
+          }
         ]
       },
+
     })
 
     this.editor.getConfig().showDevices = false
+
+    const blockManager = this.editor.BlockManager
+
+    /*
+    blockManager.add('h1-block', {
+      label: 'Heading',
+      content: '<h1>Put your title here</h1>',
+      category: 'Basic',
+     
+      attributes: {
+      title: 'Insert h1 block',
+     
+      }
+    })
+    */
 
   }
 }
